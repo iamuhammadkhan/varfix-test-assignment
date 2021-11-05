90e69d943795af81aff3a07451019038dd6a8c7a
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-11-06 02:06:47 +0500

    Initial Commit

---
+++ Varfix-Test-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Varfix-Test-Assignment
+//
+//  Created by Muhammad Khan on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
